00f8cfdf0b7f7e306f82fae838a908770fc9ceb9
rodrigosanchone <rodrigosanchonet@outlook.es>
2026-05-09 20:29:20 -0600

    Cambio a GTM-TQN77VWJ

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,18 +82,18 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T3LJMXT6');
-          `}
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TQN77VWJ');
+        `}
         </Script>
       </head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T3LJMXT6"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TQN77VWJ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
